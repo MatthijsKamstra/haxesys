e47d7ff4da7b7d864466714140f535f28492ed79
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-06-11 10:56:21 +0200

    use templates from haxe with settings file

--- 10markdown/code/src/Main.hx
+++ 10markdown/code/src/Main.hx
@@ -10,6 +10,8 @@ class Main {
 	private static var ASSETS:String; // root folder of the website
 	private static var WWW:String; // folder to generate files in (in this case `docs` folder from github )
 
+	var settings:Dynamic; // should make this a typedef
+
 	function new() {
 		var startTime = Date.now().getTime(); // lets see how fast target really are
 
@@ -17,13 +19,19 @@ class Main {
 		WWW = Path.normalize(Sys.getCwd().split('bin/')[0] + '/docs/${TARGET}'); // normal situation this would we just the `www` or `docs` folder
 		ASSETS = Path.normalize(Sys.getCwd().split('bin/')[0] + '/assets/');
 
+		// create some general information/settings which can be used for template generation
+		settings = {
+			title: '[${TARGET}] minimal static site generator',
+			footer: 'Copyright &copy; ${Date.now().getFullYear()} - [mck]'
+		}
+
 		trace('[${TARGET}] Creating a static site generator');
 
 		writeIndex();
 		writePosts();
 		writePages();
 
-		trace('[${TARGET}] done in ${Date.now().getTime() - startTime}ms');
+		trace('[${TARGET}] done in ${Std.int(Date.now().getTime() - startTime)}ms');
 	}
 
 	function writeIndex() {
@@ -75,16 +83,18 @@ class Main {
 	}
 
 	function createHead():String {
-		var head = '
+		var str = '
 		<head>
 			<!-- Required meta tags -->
 			<meta charset="utf-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 			<!-- Bootstrap CSS -->
 			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-			<title>Hello, world!</title>
+			<title>::title::</title>
 		</head>';
-		return head;
+		var template = new haxe.Template(str);
+		var output = template.execute(settings);
+		return output;
 	}
 
 	function createNavigation():String {
@@ -93,14 +103,16 @@ class Main {
 	}
 
 	function createFooter():String {
-		var str = '<footer>\n<!-- footer -->\n</footer>
+		var str = '<footer>\n::footer::\n</footer>
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 		';
-		return str;
+		var template = new haxe.Template(str);
+		var output = template.execute(settings);
+		return output;
 	}
 
 	static public function main() {
