e9e05ac62e02dca500e47505c630ae65fd7838bd
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-06-18 13:32:22 +0200

    updated docs

--- docs/14cli_progress/code/src/Main.hx
+++ docs/14cli_progress/code/src/Main.hx
@@ -24,17 +24,26 @@ class Main {
 	 */
 	function progressBar(current:Int, total:Int){
 		var _fill = '█';
+		#if python
+		// python really really doens't like this fill character
+		_fill = 'X';
+		#end
+		var _empty = '-';
 		var _current = '';
 		var _rest = '';
 		for(i in 0...current){
 			_current += _fill;
 		}
 		for(i in 0...(total-current)){
-			_rest += '-';
+			_rest += _empty;
 		}
 		var progress = '|${_current}${_rest}| ${Std.int((current/total)*100)}%';
-		Sys.stdout().writeString ('\r'+progress);
+		Sys.stdout().writeString ('\r'+progress+' ');
 		Sys.stdout().flush();
+		#if lua
+		// really weird, but lua needs this to update the cli
+		Sys.print('');
+		#end
 	}
 
 	static public function main() {
