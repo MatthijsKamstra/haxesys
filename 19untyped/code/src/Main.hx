eedf189315d1191a2bd2dd039fbb17bfb0110f53
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-08-27 17:22:46 +0200

    untyped example

---
+++ 19untyped/code/src/Main.hx
@@ -0,0 +1,68 @@
+package;
+
+
+/**
+ * @author Matthijs Kamstra aka [mck]
+ */
+class Main {
+
+	// defaults
+	var TARGET:String = ''; // current target (neko, node.js, c++, c#, python, java)
+
+
+	public function new() {
+
+		TARGET = Sys.getCwd().split('bin/')[1].split('/')[0]; // yep, that works in this folder structure
+
+		trace('[${TARGET}] untyped');
+
+		var myMessage = "Haxe is great!";
+
+		#if js
+			Sys.println('[${TARGET}]');
+			untyped __js__('// [js] comment');
+			js.Node.console.log('js.Node.console.log');
+			untyped __js__('console.log({0})', myMessage);
+			untyped ('// [${TARGET}] simple untyped');
+		#elseif php
+			Sys.println('[${TARGET}]');
+			untyped __php__('// [php] comment');
+			untyped __php__("echo '<pre>'; print_r($myMessage); echo '</pre>';");
+			untyped ('// [${TARGET}] simple untyped');
+		#elseif java
+			Sys.println('[${TARGET}]');
+			untyped __java__('// [java] comment');
+			untyped ('// [${TARGET}] simple untyped');
+		#elseif cs
+			Sys.println('[${TARGET}]');
+			untyped __cs__('// [cs] comment');
+			untyped __cs__("// {0} is bool", myMessage);
+			untyped ('// [${TARGET}] simple untyped');
+		#elseif cpp
+			Sys.println('[${TARGET}]');
+			untyped __cpp__('// [cpp] comment');
+			untyped ('// [${TARGET}] simple untyped');
+		#elseif nodejs
+			Sys.println('[${TARGET}]');
+			untyped __js__('// [nodejs] comment');
+			js.Node.console.log('js.Node.console.log');
+			untyped ('// [${TARGET}] simple untyped');
+		#elseif python
+			// untyped __python__('# [python] comment');
+			untyped ('# [python] comment');
+		#elseif lua
+			Sys.println('[${TARGET}]');
+			untyped __lua__('-- [lua] comment');
+			untyped ('// [${TARGET}] simple untyped');
+		#elseif neko
+			// untyped __neko__('// [neko] comment');
+			untyped ('// [${TARGET}] simple untyped');
+		#else
+			untyped ('// haxe lsss');
+		#end
+	}
+
+	static public function main() {
+		var app = new Main();
+	}
+}
