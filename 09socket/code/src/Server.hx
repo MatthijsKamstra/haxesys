9173a598b0b0b265f2a556cd9f627f6d3e7986de
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-10-17 13:42:25 +0200

    working on sockets again!

--- 09socket/code/src/Server.hx
+++ 09socket/code/src/Server.hx
@@ -7,29 +7,42 @@ class Server {
 	var TARGET:String;
 
 	public function new() {
-		TARGET = Sys.getCwd().split('bin/')[1].split('/')[0]; // yep, that works in this folder structure
+		TARGET = Sys.getCwd().split('bin/server/')[1].split('/')[0]; // yep, that works in this folder structure
 
 		trace('Server - ${TARGET}');
 		Sys.println('Server - ${TARGET}');
-		Sys.stdout().writeString('Server - ${TARGET}');
+		// Sys.stdout().writeString('Server - ${TARGET}');
 
+		var port = 5000;
 		var host = new Host(Host.localhost());
+
 		var socket = new Socket();
-		socket.bind(host, 5000);
-		socket.listen(1);
+		socket.bind(host, port);
+		socket.listen(10);
 
 		trace('Starting server... ${host}, ${socket.host()}');
 		while (true) {
 			var c:sys.net.Socket = socket.accept();
-			trace('Client connected... ${c.host()}');
-			Sys.println('Client connected... ${c.host()}');
-			Sys.stdout().writeString('Client connected... ${c.host()}');
-			c.write("hello !\n");
-			c.write('Server - $TARGET \n');
-			c.write("Current date: " + Date.now() + "\n");
-			c.write("your IP is " + c.peer().host.toString() + "\n");
-			c.write("exit");
-			c.close();
+			if (c != null) {
+				try {
+					socket.setBlocking(false);
+				} catch (e:Dynamic) {
+					trace(e);
+					var output:sys.io.FileOutput = sys.io.File.append('error.txt', false);
+					output.writeString('$e');
+					output.close();
+				}
+				trace('Client connected... ${c.host()}');
+				Sys.println('Client connected... ${c.host()}');
+				Sys.stdout().writeString('Client connected... ${c.host()}');
+				c.write("hello !\n");
+				c.write('Server - $TARGET \n');
+				c.write("Current date: " + Date.now() + "\n");
+				c.write("your IP is " + c.peer().host.toString() + "\n");
+				c.write("your: " + c.peer() + "\n");
+				c.write("exit");
+				c.close();
+			}
 		}
 	}
 
